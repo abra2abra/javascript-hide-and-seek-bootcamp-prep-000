aacfe98e3528bdc710e8022cba207c74c6e55b39
abra2abra <abra2abra@mailinator.com>
2018-03-12 10:53:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,5 +19,5 @@ function increaseRankBy(n){
 function deepestChild(){
         var divs = document.getElementById('grand-node').querySelectorAll('div');
     
-    return divs[3];
+    return divs[divs.length -1];
 }
